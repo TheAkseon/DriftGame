7c6c0c47acbc47d3763aaca9bdc61539c596bb21
Andre0y <Andreysirotkin2009@gmail.com>
2024-06-10 19:26:35 +0300

    Добавил переключение между уровнями, код пока что не очень

--- Assets/Applications/Scripts/Level/LevelBehaviour.cs
+++ Assets/Applications/Scripts/Level/LevelBehaviour.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class LevelBehaviour : MonoBehaviour
 {
+    [SerializeField] private GameObject _driftLevelsUI;
+    [SerializeField] private GameObject _freeRideLevelsUI;
+    [SerializeField] private GameObject _gameModesUI;
+
     private int _currentLevel = 1;
     private int _nextLevel;
 
-    public void NextLevel()
+    public void LoadNextScene()
     {
         _nextLevel = _currentLevel + 1;
 
@@ -14,7 +18,41 @@ public class LevelBehaviour : MonoBehaviour
         _currentLevel = _nextLevel;
     }
 
-    public void PreviousLevel()
+    public void LoadFirstScene()
+    {
+        LevelLoader.Instance.UnloadScene();
+        LevelLoader.Instance.LoadLevel(1);
+    }
+
+    public void LoadLevelsUI(bool isDrift)
+    {
+        if (isDrift)
+        {
+            _gameModesUI.SetActive(false);
+            _driftLevelsUI.SetActive(true);
+        }
+        else
+        {
+            _gameModesUI.SetActive(false);
+            _freeRideLevelsUI.SetActive(true);
+        }
+    }
+
+    public void LoadGameModesUI(bool isDrift)
+    {
+        if (isDrift)
+        {
+            _driftLevelsUI.SetActive(false);
+            _gameModesUI.SetActive(true);
+        }
+        else
+        {
+            _freeRideLevelsUI.SetActive(false);
+            _gameModesUI.SetActive(true);
+        }
+    }
+
+    public void LoadPreviousLevel()
     {
         _nextLevel = _currentLevel - 1;
         LevelLoader.Instance.UnloadScene();
