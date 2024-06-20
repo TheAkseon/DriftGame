dc211ff128b2c4fc0acc1df7de20a986e24e8734
Kirill <abqwerty390@gmail.com>
2024-06-20 16:28:51 +0300

    добавил скрипт для выхода в главное меню

--- Assets/Applications/Scripts/Level/Load/LevelNavigationHandler.cs
+++ Assets/Applications/Scripts/Level/Load/LevelNavigationHandler.cs
@@ -1,21 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LevelNavigationHandler : MonoBehaviour
 {
     [SerializeField] private Button _restartButton;
+    [SerializeField] private Button _menuButton;
 
+    private int _menuIndex = 1;
     private int _levelIndex = 2;
 
     private void Start()
     {
         _restartButton.onClick.AddListener(RestartLevel);
+        _menuButton.onClick.AddListener(ExitToMenu);
     }
 
     private void RestartLevel()
     {
         LevelLoader.Instance.LoadLevel(_levelIndex);
+    }
+
+    private void ExitToMenu()
+    {
+        LevelLoader.Instance.LoadLevel(_menuIndex);
     }
 }
