27702718c3ef35c108582d38a9d0d7d9bd76eb6f
Kirill <abqwerty390@gmail.com>
2024-06-20 16:25:53 +0300

    сделал скрипт для рестарта уровня

--- Assets/Applications/Scripts/Level/Load/LevelNavigationHandler.cs
+++ Assets/Applications/Scripts/Level/Load/LevelNavigationHandler.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelNavigationHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button _restartButton;
+
+    private int _levelIndex = 2;
+
+    private void Start()
     {
-        
+        _restartButton.onClick.AddListener(RestartLevel);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RestartLevel()
     {
-        
+        LevelLoader.Instance.LoadLevel(_levelIndex);
     }
 }
